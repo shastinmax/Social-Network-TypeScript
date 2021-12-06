d777b7aa8c1f0bfde6e337011908da1a7f52cc5f
Shastin Maksim <shastin_maksim@mail.ru>
2021-12-06 12:33:20 +0300

    add component Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,22 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 
 function App() {
     return (
         <div className='app-wrapper'>
-           <Header/>
-            <nav className='nav'>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-
-            </nav>
+            <Header/>
+            <Navbar/>
             <div className='content'>
                 <div>
-                    <img src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300' />
+                    <img
+                        src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'/>
                 </div>
                 <div className='avatar'>
-                    <img src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" alt='avatar'/>
+                    <img src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+                         alt='avatar'/>
                     descrip
                 </div>
                 <div>
