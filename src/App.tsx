fbecd69b64b561a83853883e2e2cbec4fc0b7f96
Shastin Maksim <shastin_maksim@mail.ru>
2021-12-06 11:44:37 +0300

    add component App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img className='logo'
+                     src='http://pngimg.com/uploads/magic_hat/small/magic_hat_PNG102.png'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300' />
+                </div>
+                <div className='avatar'>
+                    <img src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" alt='avatar'/>
+                    descrip
+                </div>
+                <div>
+                    My Posts
+                    <div>
+                        new Post
+                    </div>
+                    <div>Post1</div>
+                    <div>Post2</div>
+                    <div>Post3</div>
+                    <div>Post4</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
