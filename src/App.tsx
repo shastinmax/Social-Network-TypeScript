0c596146d5b681decf3d4efb92eb6bd813b0b86d
Shastin Maksim <shastin_maksim@mail.ru>
2021-12-09 23:28:48 +0300

    add Route for Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,28 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter,Routes, Route} from "react-router-dom";
 
 
 
 function App() {
+
     return (
-        <div className='app-wrapper'>
+        <BrowserRouter>
+            <div className='app-wrapper'>
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                <Profile/>
-                {/*<Dialogs/>*/}
+                <Routes>
+                    <Route path='/profile' element={<Profile/>}/>
+                    <Route path='/message' element={<Dialogs/>}/>
+                </Routes>
+
             </div>
 
         </div>
+        </BrowserRouter>
+
     );
 }
 
