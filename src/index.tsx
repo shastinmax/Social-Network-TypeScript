26ad98ca4f763bb9e78c6050bd51ce1bc54e4923
Shastin Maksim <shastin_maksim@mail.ru>
2021-12-28 16:52:35 +0300

    add subscribe

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {state} from "./redux/state";
-import {rerenderEntireTree} from "./render";
+import {addPost, state, StateType, subscribe, updateNewPostText} from "./redux/state";
+import ReactDOM from "react-dom";
+import App from "./App";
+// type RerenderEntireTreeType=(state:StateType)=>void
+
+
+let rerenderEntireTree = (state: StateType) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
 rerenderEntireTree(state)
+subscribe(rerenderEntireTree)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,21 +1,23 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = (state: StateType) => {
+    console.log(state)
+}
 
 export type RouteType = {
     path: string
     title: string
-    id:number
+    id: number
 
 }
 export type DialogType = {
     pathDialog: string
     name: string
     dialog: string
-    id:number
+    id: number
 }
 export type PostsType = {
     message: string | undefined
     likesCount: number
-    id:number
+    id: number
 }
 export type ProfileType = {
     routes: Array<RouteType>
@@ -24,7 +26,7 @@ export type ProfileType = {
 
 export type DialogsType = {
     posts: Array<PostsType>
-    newPostText:string
+    newPostText: string
     dialog: Array<DialogType>
 }
 
@@ -39,29 +41,29 @@ export type StateType = {
     dialogsPage: DialogsType
 
 }
-export type UpdateNewPostTextType=(title:string)=>void
+export type UpdateNewPostTextType = (title: string) => void
 
 export const state: StateType = {
     profilePage: {
         routes: [
-            {id : 1, path: '/profile', title: 'Profile'},
-            {id : 2, path: '/dialogs', title: 'Message'},
-            {id : 3, path: '/news', title: 'News'},
-            {id : 4, path: '/music', title: 'Music'},
-            {id : 5, path: '/settings', title: 'Settings'}],
+            {id: 1, path: '/profile', title: 'Profile'},
+            {id: 2, path: '/dialogs', title: 'Message'},
+            {id: 3, path: '/news', title: 'News'},
+            {id: 4, path: '/music', title: 'Music'},
+            {id: 5, path: '/settings', title: 'Settings'}],
 
     },
     dialogsPage: {
         posts: [
-            {id : 1, message: "Hello", likesCount: 15},
-            {id : 2, message: "Yo", likesCount: 7},
-            {id : 3, message: "Goodbye", likesCount: 0}],
-        newPostText:'it-kamas',
+            {id: 1, message: "Hello", likesCount: 15},
+            {id: 2, message: "Yo", likesCount: 7},
+            {id: 3, message: "Goodbye", likesCount: 0}],
+        newPostText: 'it-kamas',
         dialog: [
-            {id : 1, pathDialog: '/dialogs/1', name: 'Slava', dialog: 'Hello'},
-            {id : 2, pathDialog: '/dialogs/2', name: 'Borya', dialog: 'YO-YO'},
-            {id : 3, pathDialog: '/dialogs/3', name: 'Igor', dialog: 'Goodbay'},
-            {id : 4, pathDialog: '/dialogs/4', name: 'Viktor', dialog: 'YES ABHSS'}]
+            {id: 1, pathDialog: '/dialogs/1', name: 'Slava', dialog: 'Hello'},
+            {id: 2, pathDialog: '/dialogs/2', name: 'Borya', dialog: 'YO-YO'},
+            {id: 3, pathDialog: '/dialogs/3', name: 'Igor', dialog: 'Goodbay'},
+            {id: 4, pathDialog: '/dialogs/4', name: 'Viktor', dialog: 'YES ABHSS'}]
     }
 
 }
@@ -74,12 +76,15 @@ export const addPost: AddPostType = () => {
         likesCount: 0,
     }
     state.dialogsPage.posts.push(newPost)
-    state.dialogsPage.newPostText=''
+    state.dialogsPage.newPostText = ''
     rerenderEntireTree(state)
 }
 
-export const updateNewPostText: UpdateNewPostTextType = (text:string) => {
-state.dialogsPage.newPostText=text
-
+export const updateNewPostText: UpdateNewPostTextType = (text: string) => {
+    state.dialogsPage.newPostText = text
     rerenderEntireTree(state)
+}
+
+export const subscribe = (observer: any) => {
+    rerenderEntireTree = observer
 }
