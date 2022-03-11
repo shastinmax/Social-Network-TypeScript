ac8aef857b651797be6c5d3bcec3ef3fc74f14aa
Shastin Maksim <shastin_maksim@mail.ru>
2022-03-11 23:08:45 +0300

    lesson_75_Login

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -5,6 +5,7 @@ import {navbarReducer} from "./navbar-reducer";
 import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
 import thunkMiddleware from "redux-thunk"
+import {reducer as formReducer} from 'redux-form'
 
 
 let reducers=combineReducers({
@@ -13,6 +14,7 @@ let reducers=combineReducers({
     navbar:navbarReducer,
     usersPage:usersReducer,
     auth:authReducer,
+    form:formReducer
 
 })
 export type AppStateType = ReturnType<typeof reducers>
